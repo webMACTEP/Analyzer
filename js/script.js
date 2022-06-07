ce61725ccada4b3aa0172f76913a382a765dbd03
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2022-06-07 09:38:59 +0300

    mob menu

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
 
    $('.burger').click(function (event) {
       $('.b1, .b2, .subheader').toggleClass('active');
-
+      $('body').toggleClass('lock');
 
    });
 
